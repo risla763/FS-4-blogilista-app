92fcdc133bdf60ee2f21db749a2ef92ff5b4c252
Maija Rislakki <maijarislakki@Maija-MacBook-Air.local>
2026-02-06 18:09:37 +0200

    4.6

--- src/tests/first.test.js
+++ src/tests/first.test.js
@@ -158,3 +158,46 @@ describe('favourite blog is calculated right',() => {
     assert.deepStrictEqual(result,12)
   })
 })
+
+
+describe('most blogs written', () => {
+  const Blogs = [    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+
+   {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+        _id:"5a422bc61b54a676234d17fc", 
+        title:"Type wars", 
+        author:"Edsger W. Dijkstra",
+        url:"http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html", 
+        likes : 2, 
+        __v : 0
+    }
+]
+
+  test('return the author with most blogs', () => {
+    const result = listHelper.mostBlogs(Blogs)
+    assert.deepStrictEqual(result, { author: "Robert C. Martin", blogs: 2 })
+  }) 
+})
--- src/utils/list_helper.cjs
+++ src/utils/list_helper.cjs
@@ -21,8 +21,28 @@ const favouriteBlog = (blogs) => {
   return favourite
 }
 
+//most blogs? funktio joka saa parametriksi taulukollinen blogeja
+//millä kirjoittajalla on eniten blogeja? palauttaa objektin muodossa {author: "Edsger W. Dijkstra", blogs: 2}
+const mostBlogs = (blogs) => {
+  let listOfAuthors = []
+  for (let i = 0; i < blogs.length; i++)
+    listOfAuthors.push(blogs[i].author)
+  let max = 0
+  let author = ""
+  const apulista = {}
+  for (let i = 0; i < listOfAuthors.length; i++) {
+    apulista[listOfAuthors[i]] = (apulista[listOfAuthors[i]] || 0) + 1
+    if (apulista[listOfAuthors[i]] > max) {
+      max = apulista[listOfAuthors[i]]
+      author = listOfAuthors[i]
+    }
+  }
+  return { author: author, blogs: max }
+}
+
 module.exports = {
   dummy,
   totalLikes,
-  favouriteBlog
+  favouriteBlog,
+  mostBlogs
 }

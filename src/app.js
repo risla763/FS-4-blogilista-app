51c09d2c50430bbe9a57faceb2417de17fbccf49
Maija Rislakki <maijarislakki@Maija-MacBook-Air.local>
2026-03-10 20:30:13 +0200

    4.18

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import blogs from './controllers/blogs.js'
 import users from './controllers/users.js'
+import login from './controllers/login.js'
 
 const app = express()
 
 app.use(express.json())
-
+app.use('/api/login', login)
 app.use('/api/blogs', blogs)
 app.use('/api/users', users)
 
---
+++ src/controllers/login.js
@@ -0,0 +1,37 @@
+import express from 'express'
+import UserMongo from '../models/userMongo.js'
+import bcrypt from 'bcrypt'
+import jsonwebtoken from 'jsonwebtoken'
+
+
+const router = express.Router()
+
+router.post('/', async (request, response) => {
+  const { username, password } = request.body
+
+  const user = await UserMongo.findOne({ username })
+
+  
+  const passwordCorrect = user === null
+    ? false
+    : await bcrypt.compare(password, user.password)
+
+  if (!(user && passwordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password'
+    })
+  }
+
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  }
+
+  const token = jsonwebtoken.sign(userForToken, process.env.SECRET)
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name })
+})
+
+export default router
